618f04a1709e358248205ed633264d80a65f0d98
Kaustubh Sant <kaustubhsant10@gmail.com>
2014-11-27 19:13:44 -0500

    Adding GClock init as args

--- simpledb/server/Startup.java
+++ simpledb/server/Startup.java
@@ -6,7 +6,8 @@ import java.rmi.registry.*;
 public class Startup {
    public static void main(String args[]) throws Exception {
       // configure and initialize the database
-      SimpleDB.init(args[0]);
+	   int clockcounter = Integer.parseInt(args[1]);
+      SimpleDB.init(args[0],clockcounter);
       
       // create a registry specific for the server on the default port
       Registry reg = LocateRegistry.createRegistry(1099);
